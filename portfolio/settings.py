df642cbd729975ebf0001188fd135723d18c14c1
AliArefi1993 <arefi_ali90@yahoo.com>
2021-07-28 20:34:07 +0430

    Site is UP

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -134,8 +134,9 @@ STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
-
 try:
-    from local_setting import *
+    from .local_settings import *
 except ImportError:
     pass
+
+
